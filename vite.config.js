32c37caded055e6a46662a5d9621e80c1008ed29
0xramm <165845170+0xramm@users.noreply.github.com>
2026-07-14 18:54:42 +0530

    base url fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Forte-Innovations/",
+  base: "/forteinnovations.us/",
   plugins: [react()],
 });
